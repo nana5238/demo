c4a118c62218dd44a5b28341936fafd7c6e1a92f
na bai <na bai>
2016-04-21 17:30:58 +0800

    first commit

---
+++ Provincial_city/my.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Administrator
+ * @Date:   2016-04-15 09:49:59
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2016-04-15 09:50:09
+ */
+
+'use strict';
+$(function() {
+
+})
